1553798fb8571551f4b388ad3f79698698a83e48
Alexander Kumancev <72394319+kumancev@users.noreply.github.com>
2022-07-30 15:25:30 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import './App.css';
+import Navbar from "./Navbar.js";
+import MainMint from "./MainMint";
+
+
+function App() {
+	const[accounts, setAccounts] = useState([]);
+
+	return (
+		<div className="overlay">
+			<div className="App">
+				<Navbar accounts={accounts} setAccounts={setAccounts} />
+				<MainMint accounts={accounts} setAccounts={setAccounts} />
+			</div>
+			<div className="moving-background"></div>
+		</div>
+	);
+}
+
+export default App; 
